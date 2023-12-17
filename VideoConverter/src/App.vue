4b9bcf2e0b4ef4b07d911db16d4993b5e7f09871
fiae21l.koch <fiae21l.koch@mm-bbs.de>
2023-12-17 13:13:18 +0100

    init

---
+++ VideoConverter/src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {ref} from "vue";
+const percentageSpeed = ref()
+
+window.addEventListener("DOMContentLoaded", () => {
+  let AudioFile = document.getElementById("AudioTrack")
+      let audioPlayer = document.getElementById("Player")
+    let sourcePlayer = document.getElementById("SourcePlayer")
+  let AudioSpeedButton = document.getElementById("AudioSpeedButton")
+  AudioFile.addEventListener("change", (evt) => {
+const file = evt.target.files[0]
+
+    if (file) {
+      const fileURL = URL.createObjectURL(file);
+      sourcePlayer.src = fileURL
+      audioPlayer.load();
+
+    }
+  })
+
+   AudioSpeedButton.addEventListener("click",  () => {
+     audioPlayer.playbackRate = parseFloat(percentageSpeed.value)
+     console.log(audioPlayer.playbackRate)
+  })
+})
+</script>
+
+<template>
+  <div>
+    <input type="file" id="AudioTrack">
+
+    <input type="text" class="" v-model="percentageSpeed">
+<audio controls id="Player">
+    <source src="" id="SourcePlayer" >
+  </audio>
+    <input type="button" value="Convert Speed" id="AudioSpeedButton">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
